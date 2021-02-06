aade54e73f983f740310c000e3aaf70c83042ed7
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-02-06 00:24:39 -0500

    Remove unused prototypes

--- 2_Multidimensional_serial/flip.h
+++ 2_Multidimensional_serial/flip.h
@@ -1,7 +1,4 @@
 
 #include <torch/torch.h>
 
-torch::Tensor vertical_flip(torch::Tensor input);
-torch::Tensor horizontal_flip(torch::Tensor input);
-torch::Tensor channel_flip(torch::Tensor input);
 torch::Tensor generalized_flip(torch::Tensor input, torch::IntArrayRef flip_dims);
