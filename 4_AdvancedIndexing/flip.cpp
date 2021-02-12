9667f62e5a71cda21071666866f101dc357ce0c0
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-02-11 22:02:27 -0500

    Advanced Indexing is now working

--- 4_AdvancedIndexing/flip.cpp
+++ 4_AdvancedIndexing/flip.cpp
@@ -1,98 +1,232 @@
 
 #include <torch/torch.h>
-#include <ATen/native/cpu/Loops.h>
+// #include <ATen/native/cpu/Loops.h>
+// #include <ATen/native/TensorAdvancedIndexing.h>
 
-/**
- * Convert a 1D index into an N-D one.
- *
- * \param idx a one dimensional index to convert
- * \param dims a list containing the reference N-dimensional bases.
- * \return the corresponding index in the N-dimensional reference in the form
- * of a vector.
- **/
-std::vector<int64_t> index_to_multidimensional(int64_t idx, torch::IntArrayRef dims) {
-    const int64_t size = dims.size();
-    std::vector<int64_t> result;
-    int64_t off = idx;
-    int64_t stride = 1;
-    for(int64_t i = size - 1; i > 0; i--) {
-        auto dim = dims[i];
-        int64_t next_stride = dim * stride;
-        int64_t mod = off % next_stride;
-        off -= mod;
-        int64_t div = mod / stride;
-        result.insert(result.begin(), div);
-        stride = next_stride;
+
+struct AdvancedIndex {
+  AdvancedIndex(const torch::Tensor& src, torch::TensorList indices);
+
+  torch::Tensor src;
+  std::vector<torch::Tensor> indices;
+  torch::DimVector indexed_sizes;
+  torch::DimVector indexed_strides;
+  int64_t dims_before;
+  int64_t dims_after;
+};
+
+struct Indexer {
+  Indexer(int64_t num_indexers, char** indexers, const int64_t* indexer_strides,
+          torch::IntArrayRef original_sizes, torch::IntArrayRef original_strides)
+    : num_indexers(num_indexers)
+    , indexers(indexers)
+    , indexer_strides(indexer_strides)
+    , original_strides(original_strides.data())
+    , original_sizes(original_sizes.data()) {
+    AT_ASSERT(original_strides.size() == num_indexers);
+    AT_ASSERT(original_sizes.size() == num_indexers);
+  }
+
+  int64_t num_indexers;
+  char** indexers;
+  const int64_t* indexer_strides;
+  const int64_t* original_strides;
+  const int64_t* original_sizes;
+
+  int64_t get(int64_t idx) {
+    int64_t offset = 0;
+    for (int j = 0; j < num_indexers; j++) {
+      int64_t value = *(int64_t*)&indexers[j][idx * indexer_strides[j]];
+      int64_t size = original_sizes[j];
+      if (value < -size || value >= size) {
+        TORCH_CHECK_INDEX(false, "index ", value, " is out of bounds for dimension ", j, " with size ", size);
+      }
+      if (value < 0) {
+        value += size;
+      }
+      offset += value * original_strides[j];
     }
-    int64_t final_dim = off / stride;
-    result.insert(result.begin(), final_dim);
-    return result;
+    return offset;
+  }
+};
+
+// Replace indexed dimensions in src with stride 0 and the size of the result tensor.
+// The offset in these dimensions is computed by the kernel using the index tensor's
+// values and the stride of src. The new shape is not meaningful. It's used to make
+// the shape compatible with the result tensor.
+static torch::Tensor restride_src(const torch::Tensor& src, int64_t dims_before, int64_t dims_indexed,
+                                  torch::IntArrayRef replacement_shape) {
+  auto shape = torch::DimVector(src.sizes());
+  auto strides = torch::DimVector(src.strides());
+  int64_t end = dims_before + dims_indexed;
+  shape.erase(shape.begin() + dims_before, shape.begin() + end);
+  strides.erase(strides.begin() + dims_before, strides.begin() + end);
+  shape.insert(shape.begin() + dims_before, replacement_shape.begin(), replacement_shape.end());
+  strides.insert(strides.begin() + dims_before, replacement_shape.size(), 0);
+  return src.as_strided(shape, strides);
 }
 
-/**
- * Convert an N-dimensional index into a 1-dimensional one.
- * \param multidim a vector containing the N-dimensional coordinates to convert.
- * \param dims a list containing the reference N-dimensional bases.
- * \return the corresponding index in a 1-dimensional reference.
- **/
-int64_t multidimensional_to_index(std::vector<int64_t> multidim, torch::IntArrayRef dims) {
-    int64_t idx = 0;
-    int64_t stride = 1;
-
-    for(int64_t i = dims.size() - 1; i > 0; i--) {
-        idx += multidim[i] * stride;
-        stride *= dims[i];
-    }
-    idx += multidim[0] * stride;
-    return idx;
+// Add dimensions of size 1 to an index tensor so that it can be broadcast to the result
+// shape and iterated over element-wise like the result tensor and the restrided src.
+static torch::Tensor reshape_indexer(const torch::Tensor& index, int64_t dims_before, int64_t dims_after) {
+  auto orig_shape = index.sizes();
+  auto shape = torch::DimVector();
+  shape.append(dims_before, 1);
+  shape.append(orig_shape.begin(), orig_shape.end());
+  shape.append(dims_after, 1);
+  return index.reshape(shape);
 }
 
-/**
- * Flip a N-dimensional coordinate, given a set of dimensions to flip and the N-dimensional reference bases.
- * \param multidim a vector containing the N-dimensional coordinates to flip.
- * \param dims a list containing the reference N-dimensional bases.
- * \param flip_dims a list containing the index positions to flip.
- * \return a vector containing the flipped N-dimensional coordinates.
- **/
-std::vector<int64_t> flip_dimensions(std::vector<int64_t> multidim, torch::IntArrayRef dims, torch::IntArrayRef flip_dims) {
-    for(int64_t dim : flip_dims) {
-        multidim[dim] = dims[dim] - multidim[dim] - 1;
+
+static bool all_strides_match(torch::TensorList tensors) {
+  TORCH_CHECK(tensors.size() >= 1);
+  auto strides = tensors[0].strides();
+  for (auto& tensor : tensors.slice(1)) {
+    if (!strides.equals(tensor.strides())) {
+      return false;
     }
-    return multidim;
+  }
+  return true;
 }
 
-/**
- * Precompute indices used to perform flipping in parallel.
- * \param indices a 1D tensor containing all the indices to flip.
- * \param dims a list containing the reference dimensions of the tensor to flip.
- * \param flip_dims a list containing the index positions to flip.
- * \returns a 1D tensor containing the flipped indices.
- **/
-torch::Tensor build_indices(torch::Tensor indices, torch::IntArrayRef dims, torch::IntArrayRef flip_dims) {
-    torch::TensorIteratorConfig config;
-    torch::Tensor output_tensor;
-    auto iter =
-        config.check_all_same_dtype(false)
-            .declare_static_dtype_and_device(
-                indices.scalar_type(),
-                indices.device())
-            .add_output(output_tensor)
-            .add_input(indices)
-            .build();
+AdvancedIndex::AdvancedIndex(const torch::Tensor& src, torch::TensorList indices_list)
+{
+  int64_t element_size_bytes = src.element_size();
+  int64_t dims_before = 0, dims_after = 0, dims_indexed = 0;
+  torch::IntArrayRef replacement_shape;
+  for (size_t dim = 0; dim < indices_list.size(); dim++) {
+    if (!indices_list[dim].defined()) {
+      if (dims_indexed == 0) {
+        dims_before++;
+      } else {
+        dims_after++;
+      }
+    } else {
+      dims_indexed++;
+      replacement_shape = indices_list[dim].sizes();
+      indexed_sizes.push_back(src.size(dim));
+      indexed_strides.push_back(src.stride(dim) * element_size_bytes);
+    }
+  }
 
-    torch::native::cpu_kernel(iter, [&](int64_t idx) -> int64_t {
-        auto multi_dims = index_to_multidimensional(idx, dims);
-        auto flipped_dims = flip_dimensions(multi_dims, dims, flip_dims);
-        auto flipped_idx = multidimensional_to_index(flipped_dims, dims);
-        return flipped_idx;
-    });
+  // Check if the indexed subspace contains a dim of size 0, but the replacement
+  // shape does not. This implies that an index is out of bounds, because there
+  // is no number that's a valid index for an empty tensor. Normally, out of
+  // bounds is handled in the indexing kernel, but this case fails earlier in
+  // restride_src with an unhelpful error message.
+  if (std::find(indexed_sizes.begin(), indexed_sizes.end(), 0) != indexed_sizes.end() &&
+      std::find(replacement_shape.begin(), replacement_shape.end(), 0) == replacement_shape.end()) {
+    TORCH_CHECK_INDEX(false, "index is out of bounds for dimension with size 0");
+  }
 
-    auto start = std::chrono::steady_clock::now();
-    auto output = iter.output();
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout << "Flipped indices Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
-    return output;
+  this->dims_before = dims_before;
+  this->dims_after = dims_after;
+  this->src = restride_src(src, dims_before, dims_indexed, replacement_shape);
+
+  for (auto& index : indices_list) {
+    if (index.defined()) {
+      indices.push_back(reshape_indexer(index, dims_before, dims_after));
+    }
+  }
+
+  // For CUDA tensors, force all index tensors to have the same striding to
+  // simplify the CUDA kernel.
+  if (indices.size() >= 2 && this->src.device().type() == torch::kCUDA) {
+    if (!all_strides_match(indices)) {
+      for (size_t i = 0; i < indices.size(); i++) {
+        indices[i] = indices[i].contiguous();
+      }
+    }
+  }
+}
+
+
+torch::Tensor create_index(int64_t dim, torch::Tensor input) {
+    auto dim_index = torch::arange(input.size(dim) - 1, -1, -1).to(
+        input.device());
+    for(int64_t i = 0; i < dim; i++) {
+        dim_index = dim_index.unsqueeze(0);
+    }
+    for(int64_t i = dim + 1; i < input.dim(); i++) {
+        dim_index = dim_index.unsqueeze(-1);
+    }
+    return dim_index.expand_as(input);
+}
+
+
+std::tuple<torch::Tensor, std::vector<torch::Tensor>> build_indices(torch::Tensor input, torch::IntArrayRef flip_dims) {
+    std::vector<torch::Tensor> result;
+    std::vector<torch::Tensor> empty_result;
+    std::vector<int64_t> sizes;
+    std::vector<int64_t> empty_sizes;
+
+    const int64_t* dim_ptr = flip_dims.begin();
+
+    // Create indices for each declared dimension and empty ones where there are not dimensions
+    for(int64_t i = 0; i < input.dim(); i++) {
+        auto dim = *dim_ptr;
+        if(i == dim) {
+            auto index = create_index(dim, input);
+            result.push_back(index);
+            sizes.push_back(i);
+        } else {
+            empty_result.emplace_back();
+            empty_sizes.push_back(i);
+        }
+    }
+
+    // Join both defined and empty indices in order to permute the input tensor
+    result.insert(result.end(),
+                  std::make_move_iterator(empty_result.begin()),
+                  std::make_move_iterator(empty_result.end()));
+
+    sizes.insert(sizes.end(),
+                 std::make_move_iterator(empty_sizes.begin()),
+                 std::make_move_iterator(empty_sizes.end()));
+
+    return std::make_tuple(input.permute(sizes), std::move(result));
+}
+
+static torch::TensorIterator make_index_iterator(const AdvancedIndex& info) {
+  torch::TensorIteratorConfig config;
+  config.set_check_mem_overlap(false)
+        .check_all_same_dtype(false)
+        .declare_static_dtype_and_device(info.src.scalar_type(), info.src.device())
+        .add_output(torch::Tensor())
+        .add_input(info.src);
+  for (auto& index : info.indices) {
+    config.add_input(index);
+  }
+  return config.build();
+}
+
+void index_kernel(torch::TensorIterator& iter, torch::IntArrayRef index_size,
+                  torch::IntArrayRef index_stride, bool serial_execution=false) {
+    using scalar_t = uint8_t;
+    int ntensor = iter.ntensors();
+    // When launch the index parallel version, set a relative samll grain size less than the INTERNAL::GRAIN_SIZE
+    // to make the whole available thread numbers get more balanced work load and a better cache location.
+    // The grain size here is chosen by the op benchmark to overcome the thread launch overhead
+    const int index_parallel_grain_size = 3000;
+    auto loop = [&](char** data, const int64_t* strides, int64_t n) {
+        auto indexer = Indexer(ntensor - 2, &data[2], &strides[2], index_size, index_stride);
+        char* dst = data[0];
+        char* src = data[1];
+
+        // int64_t offset = indexer.get(0);
+
+        for (int64_t i = 0; i < n; i++) {
+            int64_t offset = indexer.get(i);
+            *(scalar_t*)(dst + strides[0] * i) = *(scalar_t*)(src + strides[1] * i + offset);
+        }
+    };
+
+    if (serial_execution) {
+        iter.serial_for_each(loop, {0, iter.numel()});
+    } else {
+        iter.for_each(loop, index_parallel_grain_size);
+    }
+
 }
 
 /**
@@ -101,61 +235,19 @@ torch::Tensor build_indices(torch::Tensor indices, torch::IntArrayRef dims, torc
  * \param flip_dims a list of dimensions used to flip the tensor.
  **/
 torch::Tensor generalized_flip(torch::Tensor input, torch::IntArrayRef flip_dims) {
-    auto sizes = input.sizes();
-    auto start = std::chrono::steady_clock::now();
-    auto indices = torch::arange(input.numel());
-    auto indices_end = std::chrono::steady_clock::now();
+    // std::sort(flip_dims.begin(), flip_dims.end());
+    std::vector<int64_t> dims;
+    for(int64_t dim: flip_dims) {
+        dims.push_back(dim);
+    }
 
-    auto start_flip = std::chrono::steady_clock::now();
-    auto flipped_indices = build_indices(indices, input.sizes(), flip_dims);
-    auto end = std::chrono::steady_clock::now();
+    std::sort(dims.begin(), dims.end());
 
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::chrono::duration<double> flip_elapsed_seconds = end - start_flip;
-    std::chrono::duration<double> arange_elapsed_seconds = indices_end - start;
-    std::cout << "Arange Elapsed time (ms): " << arange_elapsed_seconds.count() * 1000 << std::endl;
-    std::cout << "Flip indices Elapsed time (ms): " << flip_elapsed_seconds.count() * 1000 << std::endl;
-    std::cout << "Indices Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
+    std::vector<torch::Tensor> indices;
+    std::tie(input, indices) = build_indices(input, dims);
 
-    // std::cout << "Flipped indices size: " << flipped_indices.sizes() << "\n";
-    // std::cout << "Indices size: " << indices.sizes() << "\n";
-
-    torch::TensorIteratorConfig config;
-    torch::Tensor output_tensor;
-    auto iter =
-        config.check_all_same_dtype(false)
-            .declare_static_dtype_and_device(
-                input.scalar_type(),
-                input.device())
-            .add_output(output_tensor)
-            .add_input(input)
-            .add_input(indices.as_strided(input.sizes(), input.strides()))
-            .add_input(flipped_indices.as_strided(input.sizes(), input.strides()))
-            .build();
-
-    using scalar_t = uint8_t;
-    // using Vec = torch::vec256::Vec256<scalar_t>;
-    auto loop = [&](char **data, const int64_t *strides, int64_t n) {
-        scalar_t *dst = (scalar_t *)data[0];
-        scalar_t *src = (scalar_t *)data[1];
-        int64_t *indices = (int64_t*)data[2];
-        int64_t *flipped_indices = (int64_t*)data[3];
-
-        int64_t base_index = indices[0];
-        for (int64_t i = 0; i < n; i++)
-        {
-            auto flip_index = flipped_indices[i];
-            auto cur_index = indices[i];
-            *(dst - base_index + cur_index) = *(src - base_index + flip_index);
-        }
-    };
-
-    // iter.serial_for_each(loop, {0, iter.numel()});
-    start = std::chrono::steady_clock::now();
-    iter.for_each(loop);
-    auto output = iter.output();
-    end = std::chrono::steady_clock::now();
-    elapsed_seconds = end - start;
-    std::cout << "For each Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
-    return output;
+    auto advanced_index = AdvancedIndex(input, indices);
+    auto iter = make_index_iterator(advanced_index);
+    index_kernel(iter, advanced_index.indexed_sizes, advanced_index.indexed_strides);
+    return iter.output();
 }
--- 4_AdvancedIndexing/main.cpp
+++ 4_AdvancedIndexing/main.cpp
@@ -16,12 +16,13 @@ int main()
     auto tensor = torch::arange(dim0 * dim1 * dim2 * dim3, opts);
     auto reshaped_tensor = tensor.reshape({dim0, dim1, dim2, dim3}).contiguous();
     std::cout << "Input: " << reshaped_tensor << "\n\n";
-    // auto start = std::chrono::steady_clock::now();
-    // auto gen_multiflip = generalized_flip(reshaped_tensor, {1, 2, 3});
-    // auto end = std::chrono::steady_clock::now();
 
-    // std::chrono::duration<double> elapsed_seconds = end - start;
-    // std::cout << "Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
+    auto start = std::chrono::steady_clock::now();
+    auto gen_multiflip = generalized_flip(reshaped_tensor, {1, 2, 3});
+    auto end = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
 
     torch::optional<torch::Tensor> dim0_dims;
     torch::optional<torch::Tensor> dim1_dims = torch::arange(dim1 - 1, -1, -1).unsqueeze(-1).unsqueeze(-1);
@@ -32,10 +33,10 @@ int main()
     std::cout << "Dim2: " << dim2_dims.value() << std::endl;
     std::cout << "Dim3: " << dim3_dims.value() << std::endl;
 
-    auto start = std::chrono::steady_clock::now();
+    start = std::chrono::steady_clock::now();
     auto index_flip = torch::index(reshaped_tensor, {dim0_dims, dim1_dims, dim2_dims, dim3_dims});
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    end = std::chrono::steady_clock::now();
+    elapsed_seconds = end - start;
     std::cout << "Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
 
     std::cout << "Flipped!\n";
@@ -48,7 +49,7 @@ int main()
     std::cout << "Elapsed time (ms): " << elapsed_seconds.count() * 1000 << std::endl;
     // std::cout << "Multiflip: " << multiflip << "\n";
 
-    if(!multiflip.allclose(index_flip)) {
+    if(!multiflip.allclose(index_flip) && !multiflip.allclose(gen_multiflip)) {
         std::cout << "Error! Implementation values differ!" << "\n";
     } else {
         std::cout << "Values are the same!" << "\n";
